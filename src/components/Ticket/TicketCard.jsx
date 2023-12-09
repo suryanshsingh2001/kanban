bc177c21e5ee8f36d1fa155938b1a20a381896e6
suryanshsingh2001 <ricochetthestoryteller2001@gmail.com>
2023-12-09 17:07:49 +0530

    Style TicketCard

--- src/components/Ticket/TicketCard.jsx
+++ src/components/Ticket/TicketCard.jsx
@@ -12,23 +12,27 @@ const TicketCard = ({ ticket, users }) => {
 
   return (
     <div className="ticket-card">
-
-      {}
+       <div className={`priority-dot ${getPriorityClass(priority)}`} />
       <div className="profile-picture">
         <img
           src="https://randomuser.me/api/portraits/men/47.jpg"
           alt="Profile"
         />
-        {user && user.available && <div className="green-dot" />}{" "}
+        {user && user.available && <div className="green-dot" />}
       </div>
       <h2>{id}</h2>
-      <h3>{title} </h3>
+      <h3>{title}</h3>
 
-      {/* <p>User: {user ? user.name : 'Unknown User'}</p>
-      <p>Priority: {getPriorityLabel(priority)}</p>
-      <p>Status: {status}</p> */}
+      {/* Added priority dot */}
 
-      <p className="tags">Tag: {tag}</p>
+      <div className="tags">
+        {tag.map((tagItem, index) => (
+          <div key={index} className="tag">
+            {tagItem}
+          </div>
+        ))}
+      </div>
+
       {/* Add any additional ticket details you want to display */}
     </div>
   );
@@ -47,6 +51,24 @@ const getPriorityLabel = (priority) => {
       return "Low";
     case 0:
       return "No priority";
+    default:
+      return "";
+  }
+};
+
+// Helper function to get priority class based on priority level
+const getPriorityClass = (priority) => {
+  switch (priority) {
+    case 4:
+      return "urgent";
+    case 3:
+      return "high";
+    case 2:
+      return "medium";
+    case 1:
+      return "low";
+    case 0:
+      return "no-priority";
     default:
       return "";
   }
