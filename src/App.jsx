9ab91ccaad0ea3301f2e2bd1d2bd2fc77a593038
suryanshsingh2001 <ricochetthestoryteller2001@gmail.com>
2023-12-09 13:27:25 +0530

    Added Local Storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-// App.js
 import React, { useState, useEffect } from 'react';
 import KanbanBoard from './components/Kanban/KanbanBoard';
 import ControlPanel from './components/ControlPanel/ControlPanel';
 import './App.css';
 
 const App = () => {
+  // Load initial state from local storage or use default values
+  const initialGroupingOption = localStorage.getItem('groupingOption') || 'status';
+  const initialSortOption = localStorage.getItem('sortOption') || 'title';
+
   const [tickets, setTickets] = useState([]);
   const [users, setUsers] = useState([]); // Add users state
-  const [groupingOption, setGroupingOption] = useState('status');
-  const [sortOption, setSortOption] = useState('title');
+  const [groupingOption, setGroupingOption] = useState(initialGroupingOption);
+  const [sortOption, setSortOption] = useState(initialSortOption);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,23 +30,26 @@ const App = () => {
 
   const handleGroupingChange = (option) => {
     setGroupingOption(option);
+    // Save to local storage
+    localStorage.setItem('groupingOption', option);
   };
 
   const handleSortChange = (option) => {
     setSortOption(option);
+    // Save to local storage
+    localStorage.setItem('sortOption', option);
   };
 
   return (
     <div className="app">
       <div className="heading">Kanban Board</div>
       <ControlPanel
-          groupingOption={groupingOption}
-          sortOption={sortOption}
-          onGroupingChange={handleGroupingChange}
-          onSortChange={handleSortChange}
-        />
+        groupingOption={groupingOption}
+        sortOption={sortOption}
+        onGroupingChange={handleGroupingChange}
+        onSortChange={handleSortChange}
+      />
       <div className="columns">
-        
         {tickets.length > 0 ? (
           <KanbanBoard tickets={tickets} groupingOption={groupingOption} sortOption={sortOption} users={users} />
         ) : (
